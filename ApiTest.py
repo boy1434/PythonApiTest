9357deb08a45d0eff22a63e87909e784d32ad6d7
Your Name <you@example.com>
2021-11-04 14:15:30 +0900

    [공공API] 4. DB연결 완료(MariaDB)

--- ApiTest.py
+++ ApiTest.py
@@ -1,13 +1,16 @@
+from os import replace
 import requests
-#import sqlalchemy as db
+import sqlalchemy as db
 import pandas as pd
 
+# 1. http 통신을 위해 requests 모듈로 데이터 다운 받기
 url = "http://openapi.seoul.go.kr:8088/41694f6b566162633832477a616341/json/RealtimeCityAir/1/5"
 response = requests.get(url)
 parseResponse = response.json()
 
 # print(parseResponse)
 
+# 2. 파이썬 list로 데이터 정제하기
 row = parseResponse["RealtimeCityAir"]["row"]
 
 weather = []
@@ -23,5 +26,11 @@ for api in row:
 
 # print(weather)
 
+# 3. Pandas로 데이터 변경하기
 weather_dataFrame = pd.DataFrame(weather)
 print(weather_dataFrame)
+
+# 4. DB연결 하기
+
+engine = db.create_engine(
+    "mariadb+mariadbconnector://python:python1234@127.0.0.1:3306/pythondb")
