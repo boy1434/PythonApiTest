7f1162cafdf26360f1c3fe9299aaae8f875fad69
Your Name <you@example.com>
2021-11-04 14:05:58 +0900

    [공공API] 2. 파이썬 list 로 데이터 정재 완료

--- ApiTest.py
+++ ApiTest.py
@@ -6,4 +6,19 @@ url = "http://openapi.seoul.go.kr:8088/41694f6b566162633832477a616341/json/Realt
 response = requests.get(url)
 parseResponse = response.json()
 
-print(parseResponse)
+# print(parseResponse)
+
+row = parseResponse["RealtimeCityAir"]["row"]
+
+weather = []
+# print(row)
+
+for api in row:
+    list = {}
+    list["MSRDT"] = api["MSRDT"]
+    list["MSRSTE_NM"] = api["MSRSTE_NM"]
+    list["PM10"] = api["PM10"]
+    list["IDEX_NM"] = api["IDEX_NM"]
+    weather.append(list)
+
+print(weather)
